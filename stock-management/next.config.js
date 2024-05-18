2213e1ae353e08bccee52fd72442253759f8ef9e
milenays <milenaystore@gmail.com>
2024-05-18 18:10:59 +0300

    asdf

---
+++ stock-management/next.config.js
@@ -0,0 +1,6 @@
+// next.config.js
+module.exports = {
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+}
---
+++ stock-management/src/pages/_app.js
@@ -0,0 +1,13 @@
+// src/pages/_app.js
+import { NextUIProvider } from '@nextui-org/react';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
+}
+
+export default MyApp;
