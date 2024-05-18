ced1e09481b60299be1d43f25223bad96ef83ed2
milenays <milenaystore@gmail.com>
2024-05-18 18:05:55 +0300

    asdf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/react/**/*.{js,ts,jsx,tsx}', // NextUI için
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
